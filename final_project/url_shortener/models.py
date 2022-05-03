4a6ec25ae2777cdac58a1cccf05180fad6e2e663
Maxim <maksim.mystafa@gmail.com>
2022-05-03 12:41:37 +0300

    add ShortURL model

--- final_project/url_shortener/models.py
+++ final_project/url_shortener/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ShortURL(models.Model):
+    """
+    A class to represent a model of short url.
+
+    Attributes
+    ----------
+    hashed_url : str,
+        hash-salted full original link
+    full_url : str,
+        full original link
+    number_of_uses : unsigned int,
+        number of uses of the short link by the user
+
+    """
+    hashed_url = models.CharField(max_length=8)
+    full_url = models.URLField()
+    number_of_uses = models.PositiveIntegerField()
+
