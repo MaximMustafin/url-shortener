c2773bb00f4709f8637d7d2fe591be71c9d7623b
Maxim <maksim.mystafa@gmail.com>
2022-05-05 23:05:13 +0300

    design home page

--- final_project/final_project/urls.py
+++ final_project/final_project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('url_shortener/', include('url_shortener.urls')),
+    path('app/', include('url_shortener.urls')),
     path('admin/', admin.site.urls),
 ]
--- final_project/url_shortener/views.py
+++ final_project/url_shortener/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Message")
+    return render(request=request, template_name='url_shortener/index.html')
